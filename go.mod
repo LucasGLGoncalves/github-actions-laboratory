bbec5e2ce738993e24819331e9eca65641894986
LucasGLGoncalves <lucas.gabrielsglgl@gmail.com>
2025-06-25 12:56:19 -0400

    go project modified

--- go.mod
+++
@@ -1,3 +0,0 @@
-module LucasGLGoncalves/github-flow
-
-go 1.24.4
--- main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	// It is strictly necessary to print 'Hello, World', otherwise you'll have bad luck in your studies :)
-	fmt.Println("Hello World!!")
-}
